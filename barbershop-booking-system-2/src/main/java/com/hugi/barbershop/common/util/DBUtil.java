de937361a0c8c5382c0506589c285d93bbeb51bd
Muhammad Adam Muqrish bin Abdul Rahim <2024235476@student.uitm.edu.my.com>
2025-06-26 15:20:19 +0800

    tukar host

--- barbershop-booking-system-2/src/main/java/com/hugi/barbershop/common/util/DBUtil.java
+++ barbershop-booking-system-2/src/main/java/com/hugi/barbershop/common/util/DBUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String DB_URL = "jdbc:oracle:thin:@//localhost:1521/FREEPDB1";
+    private static final String DB_URL = "jdbc:oracle:thin:@//192.168.0.66:1521/FREEPDB1";
     private static final String DB_USER = "project";
     private static final String DB_PASSWORD = "oracle";
 
