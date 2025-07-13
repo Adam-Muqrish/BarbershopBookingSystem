1a04b99e16a78aa2014d684b112c221f9be24718
Muhammad Adam Muqrish bin Abdul Rahim <2024235476@student.uitm.edu.my.com>
2025-07-13 08:37:37 +0800

    Update database connection details in `DBUtil.java`
    - Changed DB URL to point to 192.168.0.66
    - Updated DB user from `project` to `project2`
    - Retained DB password as `oracle`
    - Ensured auto-commit remains enabled
    - Adjusted file to remove newline at EOF

--- barbershop-booking-system-2/src/main/java/com/hugi/barbershop/common/util/DBUtil.java
+++ barbershop-booking-system-2/src/main/java/com/hugi/barbershop/common/util/DBUtil.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String DB_URL = "jdbc:oracle:thin:@//localhost:1521/FREEPDB1";
-    private static final String DB_USER = "project";
+    private static final String DB_URL = "jdbc:oracle:thin:@//192.168.0.66:1521/FREEPDB1";
+    private static final String DB_USER = "project2";
     private static final String DB_PASSWORD = "oracle";
 
     public static Connection getConnection() throws SQLException {
