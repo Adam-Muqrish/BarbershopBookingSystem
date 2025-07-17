e73df87508f9618bf0955b49a3322b7519b3cd41
Alif Aminudin <2024669628@student.uitm.edu.my>
2025-07-17 19:27:53 +0800

    update controller at staff listAppointment.java

--- barbershop-booking-system-2/src/main/java/com/hugi/barbershop/staff/controller/listAppointment.java
+++ barbershop-booking-system-2/src/main/java/com/hugi/barbershop/staff/controller/listAppointment.java
@@ -5,11 +5,15 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.util.List;
 
 import com.hugi.barbershop.common.dao.AppointmentDAO;
+import com.hugi.barbershop.common.dao.PaymentDAO;
 import com.hugi.barbershop.customer.model.Appointment;
+import com.hugi.barbershop.customer.model.Payment;
 
 /**
  * Servlet implementation class listAppointment
@@ -32,7 +36,31 @@ public class listAppointment extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		AppointmentDAO dao = new AppointmentDAO();
+		PaymentDAO paymentDAO = new PaymentDAO();
+		
+		HttpSession session = request.getSession();
+		String role = (String) request.getSession().getAttribute("staffRole");
         List<Appointment> appointments = dao.getAllAppointments();
+        List<Payment> transactions = paymentDAO.getAllPayments();
+        
+        if ("barber".equalsIgnoreCase(role)) {
+        	int barberId = Integer.parseInt((String) session.getAttribute("staffId"));
+			appointments = dao.getAllAppointmentByBarberId(barberId);
+		} else {
+			appointments = dao.getAllAppointments();
+		}
+        
+     		for (Appointment a : appointments) {
+     		    for (Payment p : transactions) {
+     		        if (p.getAppointmentId().equals(a.getAppointmentId())) {
+     		            a.setPaymentMethod(p.getPaymentMethod());
+     		            a.setPaymentStatus(p.getPaymentStatus());
+     		            break;
+     		        }
+     		    }
+     		}
+        
+        request.setAttribute("transactions", transactions);
         request.setAttribute("appointmentList", appointments);
         request.getRequestDispatcher("/WEB-INF/views/admin/listAppointment.jsp").forward(request, response);
 	}
